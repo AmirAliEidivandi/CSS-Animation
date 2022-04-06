2e20fecc77d3e6e75ab007aa462b190b076d508a
AmirAli6578 <amirah648887@gmail.com>
2022-04-06 15:50:34 +0430

    change files

--- day-12/js/script.js
+++ day-12/js/script.js
@@ -1,24 +1,36 @@
-const container = document.querySelector('.blogs')
+const container = document.querySelector(".blogs");
+const searchForm = document.querySelector(".search");
 
-const renderPost = async () => {
-    let url = "http://localhost:3000/posts";
+const renderPosts = async (term) => {
+    console.log(term);
+    let uri = "http://localhost:3000/posts?_sort=likes&_order=desc";
+    if (term) {
+        uri += `&q=${term}`;
+    }
 
-    const res = await fetch(url)
-    const posts = await res.json()
-    
-    let template = ''
-    posts.forEach(post => {
+    const res = await fetch(uri);
+    const posts = await res.json();
+    console.log(posts);
+
+    let template = "";
+    posts.forEach((post) => {
         template += `
-            <div class="post">
-                <h2>${post.title}</h2>
-                <p><samll>${post.likes} likes</samll></p>
-                <p>${post.body.slice(0, 200)}</p>
-                <a href="./detail.html?id=${post.id}">read more...</a>
-            </div>
-        `;
-    })
+      <div class="post">
+        <h2>${post.title}</h2>
+        <p><small>${post.likes} likes</small></p>
+        <p>${post.body.slice(0, 200)}...</p>
+        <a href="/detail.html?id=${post.id}">Read more</a>
+      </div>
+    `;
+    });
 
-    container.innerHTML = template
-}
+    container.innerHTML = template;
+};
 
-window.addEventListener('DOMContentLoaded', () => renderPost())
+// search
+searchForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    renderPosts(searchForm.term.value.trim());
+});
+
+window.addEventListener("DOMContentLoaded", () => renderPosts());
